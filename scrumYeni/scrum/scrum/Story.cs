cd57055cd7d776b500eef7c5055e0fc89f4c860a
4k1n <34357008+4k1n@users.noreply.github.com>
2018-05-24 15:17:00 +0300

    güncellendi

---
+++ scrumYeni/scrum/scrum/Story.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scrum
+{
+    public class Story
+    {
+        public string storyAdi;
+        public string storyYazari;
+        public string storyAciklama;
+    }
+}
---
+++ scrumYeni/scrum/scrum/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scrum
+{
+    public class Task
+    {
+        public string taskYazari;
+        public string taskAdi;
+        public string taskAciklamasi;
+        public string storyAdi;
+        public DateTime bitirmeTarihi;
+    }
+}
